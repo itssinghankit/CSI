d54bf338b5dca72156a14b741055be28df18dc1d
itssinghankit <singhankit.kr@gmail.com>
2023-03-05 14:24:35 +0530

    bottom_nav

--- app/src/main/java/com/example/csi/MainActivity.kt
+++ app/src/main/java/com/example/csi/MainActivity.kt
@@ -2,15 +2,30 @@ package com.example.csi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHost
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.csi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //for adding bottom navigation
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.ourfragments) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = binding.bottomNav
+        setupWithNavController(bottomNavigationView, navController)
+
     }
 
-
-
-    //added from ankit branch
 
 }
