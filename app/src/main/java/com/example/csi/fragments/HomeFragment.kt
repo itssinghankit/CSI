1104aa85672ddb6d0b59356622bda008887adb02
itssinghankit <singhankit.kr@gmail.com>
2023-05-12 23:22:06 +0530

    picasso

--- app/src/main/java/com/example/csi/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/csi/fragments/HomeFragment.kt
@@ -94,7 +94,6 @@ class HomeFragment : Fragment() {
             domainsrecylerview.setAlpha(true)
             domainsrecylerview.setInfinite(true)
         }
-
         Picasso.get()
             .load("http://drive.google.com/uc?export=view&id=11ObxvbYDbFXG1XzwvyCJ4P9ljntKmVCH")
             .placeholder(R.drawable.fakeimage)
