b845d2a765c7b5087febdc132fd3c9497c22b698
itssinghankit <singhankit.kr@gmail.com>
2023-05-27 17:55:46 +0530

    popup window

--- app/src/main/java/com/example/csi/Adapters/TeamFouthRecyclerAdapter.kt
+++ app/src/main/java/com/example/csi/Adapters/TeamFouthRecyclerAdapter.kt
@@ -1,7 +1,6 @@
 package com.example.csi.Adapters
 
 import android.content.Context
-import android.media.Image
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,22 +8,30 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.csi.Interfaces.OnItemClicked
 import com.example.csi.R
 import com.example.csi.modelclasses.TeamDataClassItem
 
-class TeamFouthRecyclerAdapter(val arrayList: List<TeamDataClassItem>,val context:Context) :
+class TeamFouthRecyclerAdapter(
+    val arrayList: List<TeamDataClassItem>,
+    val context: Context,
+    val listener: OnItemClicked
+) :
     RecyclerView.Adapter<TeamFouthRecyclerAdapter.ViewHolder>() {
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val personName = itemView.findViewById<TextView>(R.id.personName)
         val personDomain = itemView.findViewById<TextView>(R.id.personDomain)
-        val personImage=itemView.findViewById<ImageView>(R.id.personImage)
+        val personImage = itemView.findViewById<ImageView>(R.id.personImage)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.team_head_recycler_item, parent, false)
         val viewHolder = ViewHolder(view)
+        view.setOnClickListener {
+            listener.clickedItem(viewHolder.adapterPosition)
+        }
         return viewHolder
     }
 
@@ -35,7 +42,8 @@ class TeamFouthRecyclerAdapter(val arrayList: List<TeamDataClassItem>,val contex
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.personName.text = arrayList[position].name
         holder.personDomain.text = arrayList[position].domain
-        Glide.with(context).load(arrayList[position].video).placeholder(R.drawable.fakeimage).into(holder.personImage)
+        Glide.with(context).load(arrayList[position].video).placeholder(R.drawable.fakeimage)
+            .into(holder.personImage)
     }
 
 }
--- app/src/main/java/com/example/csi/fragments/teamFragments/FourthYearFragment.kt
+++ app/src/main/java/com/example/csi/fragments/teamFragments/FourthYearFragment.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.csi.Adapters.TeamFouthRecyclerAdapter
 import com.example.csi.Adapters.TeamMembersRecyclerAdapter
+import com.example.csi.Interfaces.OnItemClicked
 import com.example.csi.Interfaces.RetrofitInterface
 import com.example.csi.databinding.FragmentFourthYearBinding
 import com.example.csi.modelclasses.TeamDataClassItem
@@ -19,7 +20,7 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class FourthYearFragment : Fragment() {
+class FourthYearFragment : Fragment(), OnItemClicked {
     private lateinit var binding: FragmentFourthYearBinding
 
     override fun onCreateView(
@@ -33,7 +34,6 @@ class FourthYearFragment : Fragment() {
         var headArrayList= mutableListOf<TeamDataClassItem>()
         binding.teamHeadRecyclerView.layoutManager =
             LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
-
 
         //for other fourth year members
         binding.teamMemberRecyclerView.layoutManager =
@@ -54,6 +54,7 @@ class FourthYearFragment : Fragment() {
                     for (res in response.body()!!) {
                         if (res.year == "4th") {
                             membersList.add(res)
+                            membersList.add(TeamDataClassItem("instagram.com",1,"fjfjf","false","fds","Android Development","fhsj","jdkj","dfjf","Ankit singh","dfd","https://img.freepik.com/free-vector/hello-wording-comic-speech-bubble-pop-art-style_1150-39959.jpg?size=626&ext=jpg&ga=GA1.1.1107052088.1678021331&semt=ais","4th"))
                         }
                         if(res.coordinator=="TRUE"){
                             headArrayList.add(res)
@@ -61,7 +62,7 @@ class FourthYearFragment : Fragment() {
                     }
                     //for heads
                     binding.apply {
-                        binding.teamHeadRecyclerView.adapter = TeamFouthRecyclerAdapter(headArrayList,context!!)
+                        binding.teamHeadRecyclerView.adapter = TeamFouthRecyclerAdapter(headArrayList,context!!,this@FourthYearFragment)
                         teamHeadRecyclerView.set3DItem(true)
                         teamHeadRecyclerView.setAlpha(true)
                         teamHeadRecyclerView.setInfinite(true)
@@ -79,6 +80,10 @@ class FourthYearFragment : Fragment() {
         })
 
         return binding.root
+    }
+
+    override fun clickedItem(position: Int) {
+        Toast.makeText(context, position, Toast.LENGTH_SHORT).show()
     }
 
 }
