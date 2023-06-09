45365175cfe4c9107ee47e3bbd56d90e9e6955d0
itssinghankit <singhankit.kr@gmail.com>
2023-06-09 23:32:26 +0530

    login signup

--- app/src/main/java/com/example/csi/fragments/communuityFragments/CommunitySignupDetFragment.kt
+++ app/src/main/java/com/example/csi/fragments/communuityFragments/CommunitySignupDetFragment.kt
@@ -1,4 +1,4 @@
-package com.example.csi.fragments
+package com.example.csi.fragments.communuityFragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,15 +8,14 @@ import android.view.ViewGroup
 import com.example.csi.R
 
 
-class CommunitySignupFragment : Fragment() {
+class CommunitySignupDetFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_community_signup, container, false)
+        return inflater.inflate(R.layout.fragment_community_signup_det, container, false)
     }
-
 
 }
