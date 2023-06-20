a38928feab18c3f5afd28569eabc931b18f4cf14
itssinghankit <singhankit.kr@gmail.com>
2023-06-20 22:34:11 +0530

    my questions page

---
+++ app/src/main/java/com/example/csi/modelclasses/CommunityMyQuesDataClassItem.kt
@@ -0,0 +1,10 @@
+package com.example.csi.modelclasses
+
+data class CommunityMyQuesDataClassItem(
+    val body: String,
+    val createdAt: String,
+    val full_name: String,
+    val id: Int,
+    val tech: String,
+    val user: Int
+)
