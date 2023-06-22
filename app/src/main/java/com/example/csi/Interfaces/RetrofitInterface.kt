d1b6d5b132335b09f07c3de25a680f7324af537b
itssinghankit <singhankit.kr@gmail.com>
2023-06-22 18:34:50 +0530

    my questions page

--- app/src/main/java/com/example/csi/Interfaces/RetrofitInterface.kt
+++ app/src/main/java/com/example/csi/Interfaces/RetrofitInterface.kt
@@ -4,20 +4,16 @@ import com.example.csi.modelclasses.*
 
 
 import com.example.csi.modelclasses.EventDataClassItem
-import com.example.csi.modelclasses.LoginResponseDataClass
 import com.example.csi.modelclasses.TeamDataClassItem
-import okhttp3.RequestBody
-import okhttp3.ResponseBody
 
 
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Header
-import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface RetrofitInterface {
 
@@ -36,12 +32,12 @@ interface RetrofitInterface {
      fun communityIsSignedIn(@Header("Authorization")token:String):Call<CommunityIsSignedInDataClass>
 
      @POST("accounts/login/")
-     fun signedIn(@Body requestBody: CommunitySigninRequestDataClass):Call<CommunityAfterSigninDataClass>
+     fun communitySignedIn(@Body requestBody: CommunitySigninRequestDataClass):Call<CommunityAfterSigninDataClass>
 
     @GET("community/my_questions/")
-    fun myQuestions(@Header("Authorization")token:String):Call<List<CommunityMyQuesDataClassItem>>
+    fun communityMyQuestions(@Header("Authorization")token:String):Call<List<CommunityMyQuesDataClassItem>>
 
     @DELETE("/community/question_RUD/{id}")
-    fun myQuestionDelete():
+    fun communityMyQuestionDelete(@Path("id")itemId:String, @Header("Authorization")token:String):Call<Void>
 
 }
--- app/src/main/java/com/example/csi/fragments/communuityFragments/CommunityLoginFragment.kt
+++ app/src/main/java/com/example/csi/fragments/communuityFragments/CommunityLoginFragment.kt
@@ -3,7 +3,6 @@ package com.example.csi.fragments.communuityFragments
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -35,7 +34,7 @@ class CommunityLoginFragment : Fragment() {
             val signinObject=CommunitySigninRequestDataClass(binding.email.text.toString(),binding.password.text.toString())
 
             val service=RetrofitServiceBuilder.buildService(RetrofitInterface::class.java)
-            val call=service.signedIn(signinObject)
+            val call=service.communitySignedIn(signinObject)
 
             call.enqueue(object : Callback<CommunityAfterSigninDataClass?> {
                 override fun onResponse(
